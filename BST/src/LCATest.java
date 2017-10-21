4905e8eb8ff0c4472f72a804351e849d30bd6c72
coxj2 <coxj2@tcd.ie>
2017-10-21 12:09:21 +0100

    Final changes to program

--- BST/src/LCATest.java
+++ BST/src/LCATest.java
@@ -4,7 +4,8 @@ import org.junit.Test;
 
 public class LCATest {
 
-	@Test
+	@Test 
+	// Testing Constructor
 	public void testTree(){
 		LCA tree = new LCA();
 		assertNotEquals(null, tree);
@@ -17,6 +18,7 @@ public class LCATest {
 	}
 	
 	@Test
+	//Testing Node
 	public void testNode(){
 		LCA test = new LCA();
 		test.root = new Node(4);
@@ -30,11 +32,12 @@ public class LCATest {
 	@Test
 	public void testNullNode(){
 		LCA nullRootTree = new LCA();
-		nullRootTree.root = null;
 		assertEquals(nullRootTree.root, null);
 	}
 	
 	@Test
+	//Testing performance of Lowest Common Ancestor (LCA) recursive function traversing both left and right 
+	//and also testing null nodes
 	public void testFindLCA(){
 		LCA testTree = new LCA();
 		testTree.root = new Node(12);
@@ -54,7 +57,24 @@ public class LCATest {
 		assertEquals(null, lca3);
 	}
 	
+	@Test 
+	//Testing LCA function with null left and right nodes
+	public void testFindLCA2(){
+		LCA nullRightNode = new LCA();
+		nullRightNode.root = new Node(4);
+		nullRightNode.root.right = null;
+		Node lca = nullRightNode.findlca(nullRightNode.root.right, 5, 3);
+		assertEquals(null, lca);
+		
+		LCA nullLeftNode = new LCA();
+		nullLeftNode.root = new Node(8);
+		nullLeftNode.root.left = null;
+		Node lca2 = nullLeftNode.findlca(nullLeftNode.root, 5, 3);
+		assertEquals(null, lca2);
+	}
+	
 	@Test
+	//Testing the programs ability to deal with negative values
 	public void testNegativeNumbers(){
 		LCA testTree = new LCA();
 		testTree.root = new Node(0);
@@ -67,6 +87,7 @@ public class LCATest {
 	}
 	
 	@Test
+	//Testing programs ability to calculate the LCA of values that do not already exist in the tree
 	public void testNonExistentValue(){
 		LCA testTree = new LCA();
 		testTree.root = new Node(4);
@@ -74,5 +95,16 @@ public class LCATest {
 		testTree.root.left = new Node(2);
 		Node lca = testTree.findlca(testTree.root, 3, testTree.root.right.value);
 		assertEquals(4, lca.value);
+	}
+	
+	@Test
+	//Testing the programs ability to read string values in comparison with integer values
+	public void testInputError(){
+		LCA tree = new LCA();
+		tree.root = new Node(4);
+		tree.root.right = new Node(6);
+		tree.root.left = new Node(2);
+		String lca = "4";
+		assertNotEquals(4, lca);
 	}
 }
