9612dba4b7fe15a3f0c7985ffbd628a56ee83679
coxj2 <coxj2@tcd.ie>
2017-10-27 15:34:27 +0100

    Edges and Vertices Created

--- BST/src/LCA.java
+++ BST/src/LCA.java
@@ -1,64 +1,31 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
-//This is assignment 2
-class Node {
-	int value;
-	Node left;
-	Node right;
-
-	Node(int data) {
-		value = data;
-		left = null;
-		right = null;
-	}
-}
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 public class LCA {
-	Node root;
 
-	public Node findlca(Node node, int n1, int n2) {
-		if (node == null)
-			return null;
+  static class Node{
+    public final String name;
+    public final HashSet<Edge> inEdges;
+    public final HashSet<Edge> outEdges;
+    public Node(String name) {
+      this.name = name;
+      inEdges = new HashSet<Edge>();
+      outEdges = new HashSet<Edge>();
+    }
+    public Node addEdge(Node node){
+      Edge e = new Edge(this, node);
+      outEdges.add(e);
+      node.inEdges.add(e);
+      return this;
+    }
+  }
 
-		// LCA on left side
-		if (node.value > n1 && node.value > n2)
-			return findlca(node.left, n1, n2);
-
-		// LCA on right side
-		if (node.value < n1 && node.value < n2)
-			return findlca(node.right, n1, n2);
-
-		return node;
-	}
-
-	/*public static void main(String arg[]){ 
-		LCA tree = new LCA();
-		Scanner input = new Scanner(System.in);
-		try{
-		tree.root = new Node(12);
-		tree.root.left = new Node(8);
-		tree.root.right = new Node(16);
-		tree.root.left.left = new Node(6);
-		tree.root.left.right = new Node(10);
-		tree.root.right.left = new Node(14);
-		tree.root.right.right = new Node(18);
-		tree.root.left.left.left = new Node(4);
-		tree.root.left.left.right = new Node(7);
-		System.out.println("Requires an lca 0 < lca < 18");
-		System.out.println("If a value is entered that is not already present in the tree, this"
-				+ " value will be created in a new Node");
-		System.out.println("Enter the first value: ");
-		int n1 = input.nextInt();
-		System.out.println("Enter the second value: ");
-		int n2 = input.nextInt();
-		Node lca = tree.findlca(tree.root, n1, n2);
-		System.out.println("LCA of " + n1 + " and " + n2 + " is " + lca.value);
-		}
-		catch (InputMismatchException e){
-		    System.err.println("IndexOutOfBoundsException: " + "Values must be positive whole numbers " + e.getMessage());
-		}
-		catch (Exception e) {
-		    System.err.println("IndexOutOfBoundsException: " + "Values must be within set parameters " + e.getMessage());
-		} 
-	}*/
+  static class Edge{
+    public final Node from;
+    public final Node to;
+    public Edge(Node from, Node to) {
+      this.from = from;
+      this.to = to;
+    }
+  }
 }
