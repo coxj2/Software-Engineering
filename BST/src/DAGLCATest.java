f492162b42774cf196e569e2b5bc0d07800b2308
coxj2 <coxj2@tcd.ie>
2017-11-01 16:27:03 +0000

    Additional Tests added

--- BST/src/DAGLCATest.java
+++ BST/src/DAGLCATest.java
@@ -4,38 +4,44 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class DAGLCATest {
-	DAGLCA<Node> dagA = new DAGLCA<Node>();
-	ArrayList<Node> a;
-
-	
 
 	@Test
-	public void test() {
-		a = new ArrayList<>();
-		dagA = new DAGLCA<Node>();
-		//assertNull("Testing Null DAG and ArrayList",dagA.lowestCommonAncestor(a.get(9), a.get(8)));
-		
-		a = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            a.add(new Node(i));
-            }
-        dagA.setRoot(a.get(0));
-        a.get(0).addEdge(a.get(1));
-        a.get(0).addEdge(a.get(2));
-        a.get(1).addEdge(a.get(3));
-        a.get(1).addEdge(a.get(4));
-        a.get(2).addEdge(a.get(4));
-        a.get(2).addEdge(a.get(5));
-        a.get(2).addEdge(a.get(6));
-        a.get(5).addEdge(a.get(8));
-        a.get(6).addEdge(a.get(8));
-        a.get(6).addEdge(a.get(7));
-        a.get(6).addEdge(a.get(9));
-        a.get(7).addEdge(a.get(9));
-		 assertEquals("a(a9,a8) = a6", a.get(6), dagA.lowestCommonAncestor(a.get(9), a.get(8)));
-		 assertEquals("a(a8,a4) = a2", a.get(2), dagA.lowestCommonAncestor(a.get(8), a.get(4)));
-		 assertEquals("a(a8,a3) = a0", a.get(0), dagA.lowestCommonAncestor(a.get(8), a.get(3)));
+	public void testLca() {
+		DAGLCA<Node> dagA = new DAGLCA<Node>();
+		ArrayList<Node> a = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			a.add(new Node(i));
+		}
+		dagA.setRoot(a.get(0));
+		a.get(0).addEdge(a.get(1));
+		a.get(0).addEdge(a.get(2));
+		a.get(1).addEdge(a.get(3));
+		a.get(1).addEdge(a.get(4));
+		a.get(2).addEdge(a.get(4));
+		a.get(2).addEdge(a.get(5));
+		a.get(2).addEdge(a.get(6));
+		a.get(5).addEdge(a.get(8));
+		a.get(6).addEdge(a.get(8));
+		a.get(6).addEdge(a.get(7));
+		a.get(6).addEdge(a.get(9));
+		a.get(7).addEdge(a.get(9));
+		assertEquals(a.get(6), dagA.lca(a.get(9), a.get(8)));
+		assertEquals(a.get(2), dagA.lca(a.get(8), a.get(4)));
+		assertEquals(a.get(0), dagA.lca(a.get(8), a.get(3)));
+		assertNotEquals(a.get(0), dagA.lca(null, a.get(8)));
+		assertNotEquals(a.get(0), dagA.lca(a.get(8), null));
+		assertNotEquals(null, dagA.lca(a.get(8), a.get(9)));
+	}
 
+	@Test
+	public void testRoot(){
+		DAGLCA<Node> testDag = new DAGLCA<Node>();
+		assertEquals(testDag.getRoot(), null);
+		
+		ArrayList<Node> a = new ArrayList<>();
+        a.add(new Node(3));
+		testDag.setRoot(a.get(0));
+		assertNotEquals(testDag.getRoot(), null);
 	}
 
 }
